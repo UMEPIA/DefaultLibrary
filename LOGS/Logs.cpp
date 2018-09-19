0dd5bb6f44714bf5b48c718343caffa0bfb73117
u_mepia@naver.com <u_mepia@naver.com>
2018-09-19 21:43:51 +0900

    LOG 기능 추가 VERBOSE, DEBUG, INFO, ERROR, FATAL, SUCCESS의 6단계 등급

---
+++ LOGS/Logs.cpp
@@ -0,0 +1,49 @@
+#include "Logs.hpp"
+
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <stdarg.h>
+#include "qstring.h"
+#include "windows.h"
+#include <time.h>
+/*
+	TODO: Recode 부분 -> 파일에 기록 + 디버그 출력 동시에 하는 기능 추가
+*/
+void Log::Debug(const char* LOGTYPE, const char* fileName, int codeLine, const char* format, ...) {
+	char message[1024] = { 0, };
+
+	va_list lpStart;
+	va_start(lpStart, format);
+	vsprintf_s(message, 1024, format, lpStart);
+	va_end(lpStart);
+
+	time_t now = time(0);
+	struct tm  tstruct;
+	char       timeForm[80];
+	tstruct = *localtime(&now);
+	strftime(timeForm, sizeof(timeForm), "%Y-%m-%d %X", &tstruct); // YYYY-MM-DD.HH:mm:ss
+
+	char messagePrint[1024] = { 0, };
+	sprintf( messagePrint, "[%s][%s] (%s:%d) %s\n", timeForm, LOGTYPE, strrchr(fileName, '\\') ? strrchr(fileName, '\\') + 1 : fileName, codeLine , message );
+	OutputDebugStringA(messagePrint);
+}
+
+void Log::Recode(const char* LOGTYPE, const char* fileName, int codeLine, const char* format, ...) {
+	char message[1024] = { 0, };
+
+	va_list lpStart;
+	va_start(lpStart, format);
+	vsprintf_s(message, 1024, format, lpStart);
+	va_end(lpStart);
+
+	time_t now = time(0);
+	struct tm  tstruct;
+	char       timeForm[80];
+	tstruct = *localtime(&now);
+	strftime(timeForm, sizeof(timeForm), "%Y-%m-%d %X", &tstruct); // YYYY-MM-DD.HH:mm:ss
+
+	char messagePrint[1024] = { 0, };
+	sprintf(messagePrint, "[%s][VERBOSE] (%s:%d) %s\n", timeForm, strrchr(fileName, '\\') ? strrchr(fileName, '\\') + 1 : fileName, codeLine, message);
+	OutputDebugStringA(messagePrint);
+}
---
+++ Util_Def.hpp
@@ -0,0 +1,6 @@
+const char* LOGGING_TYPE_VERBOSE = "VERBOSE";
+const char* LOGGING_TYPE_DEBUG = "DEBUG";
+const char* LOGGING_TYPE_INFO = "INFO";
+const char* LOGGING_TYPE_ERROR = "ERROR";
+const char* LOGGING_TYPE_FATAL = "FATAL";
+const char* LOGGING_TYPE_SUCCESS = "SUCCESS";
